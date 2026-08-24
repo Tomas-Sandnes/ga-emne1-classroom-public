8e1d8989e466824420af9f36fcd6569d55de8b33
Tomas-Sandnes <tomas.sandnes@commodore.no>
2026-08-24 19:59:17 +0200

    Added first lesson examples

---
+++ session-01/01-lesson/02_user_input.py
@@ -0,0 +1,12 @@
+# Testing user input
+
+name = input("What is your name? ")
+course = "Emne 1"
+
+print(f"Hello, {name}!")
+print(f"Welcome to {course}.")
+
+age = int(input("How old are you? "))
+next_year = age + 1
+
+print(f"Next year you'll be {next_year}.")
